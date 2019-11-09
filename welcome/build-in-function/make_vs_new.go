7c28d68547be5243f458d57b31b024f97f30af5f
zhaoyl <htzhao200744@gmail.com>
2019-11-09 13:27:01 +0800

    [welcome] up make_vs_new

--- welcome/build-in-function/make_vs_new.go
+++ welcome/build-in-function/make_vs_new.go
@@ -5,10 +5,9 @@ import (
 )
 
 /*
-*  make, can only slice, map, channel type
-*  make, return slice, map, or channel, not point
+*  make, can only slice, map, channel type，方便append, delete等operator方法
+*  make, return slice, map, or channel, not point，方便append, delete等operator方法
 *  make, pass value, change a copy value, not self
-*  make, 以便map用make
 
 Call             Type T     Result
 make(T, n)       slice      slice of type T with length n and capacity n
@@ -27,19 +26,30 @@ make(T, n)       channel    buffered channel of type T, buffer size n
 * new, return *Type
 * new, pass point, change self
 * new, 一般struct用new
-
 new(T) , init length is 0 , cannot be index,
 */
 
 func main() {
 	// make
-	sm := make([]string, 0)
-	mapm := make(map[string]string)
+	mSlice := make([]string, 0)
+	nSlice := new([]string)
+	mSlice = append(mSlice, "a", "b", "c")
+	*nSlice = append(*nSlice, "a", "b", "c")
+	fmt.Println("mSlice: ", mSlice)
+	fmt.Println("nSlice: ", nSlice)
+
+	mMap := make(map[string]string)
+	mMap["m"] = "m"
+	nMap := new(map[string]string)
+	(*nMap)["n"] = "n"
+	fmt.Println("mMap: ", mMap)
+	fmt.Println("nMap: ", nMap)
+	delete(mMap, "m")
+	delete(*nMap, "n")
+	fmt.Println("mMap, after delete: ", mMap)
+	fmt.Println("nMap, after delete: ", nMap)
+
 	chm := make(chan int)
-	sm = append(sm, "a")
-	mapm["a"] = "a"
-	fmt.Println("sm: ", sm)
-	fmt.Println("mapm: ", mapm)
 	fmt.Println("chm: ", chm)
 
 	// new
