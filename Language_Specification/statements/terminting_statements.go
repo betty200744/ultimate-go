0f594281c0fdc3148fc73e21912ff9223242b346
zhaoyl <htzhao200744@gmail.com>
2019-12-04 21:16:24 +0800

    [Language_Specification] terminating statement

--- Language_Specification/statements/terminting_statements.go
+++ Language_Specification/statements/terminting_statements.go
@@ -1,5 +1,26 @@
 package main
 
 func main() {
+	/*
+
+	   * A "return" or "goto" statement.
+	   * A call to the built-in function panic.
+	   * A block in which the statement list ends in a terminating statement.
+	   * An "if" statement in which:
+	   	* the "else" branch is present, and
+	   	* both branches are terminating statements.
+	   * A "for" statement in which:
+	   	* there are no "break" statements referring to the "for" statement, and
+	   	* the loop condition is absent.
+	   * A "switch" statement in which:
+	   	* there are no "break" statements referring to the "switch" statement,
+	   	* there is a default case, and
+	   	* the statement lists in each case, including the default, end in a terminating statement, or a possibly labeled "fallthrough" statement.
+	   * A "select" statement in which:
+	   	* there are no "break" statements referring to the "select" statement, and
+	   	* the statement lists in each case, including the default if present, end in a terminating statement.
+	   * A labeled statement labeling a terminating statement.
+
+	*/
 
 }
