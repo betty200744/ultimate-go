0e7dc5bdd542bdd77ea9d5fc83ef19e1ca85f461
zhaoyl <htzhao200744@gmail.com>
2020-01-19 18:48:08 +0800

    [build-in-packages] type add type_conversions

--- Language_Specification/type/type_conversions.go
+++ Language_Specification/type/type_conversions.go
@@ -13,16 +13,16 @@ type Status struct {
 type FormatStatus Status
 
 func main() {
-	x, y := 3, 4
-	var f float64 = math.Sqrt(float64(x*x + y + y))
-	var z uint = uint(f)
-	fmt.Println(x, y, f, z)
-
+	x := 3
+	y := 4
+	f := math.Sqrt(float64(x*x + y + y))
+	z := uint(f)
 	s := Status{
 		Code:    1,
 		Message: "ha ha",
 	}
 	fs := (FormatStatus)(s)
+	fmt.Println(x, y, f, z)
+	fmt.Println(fmt.Sprintf("s type is : %v", reflect.TypeOf(s)))
 	fmt.Println(fmt.Sprintf("fs type is : %v", reflect.TypeOf(fs)))
-
 }
