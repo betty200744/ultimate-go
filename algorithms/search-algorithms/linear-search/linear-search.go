3d40aa6e3a2150db20abff38393c7efc9b8512df
zhaoyl <htzhao200744@gmail.com>
2020-06-01 22:25:27 +0800

    [algorithms][search-algorithms]  add linear-search.go

---
+++ algorithms/search-algorithms/linear-search/linear-search.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"gobyexample/algorithms/utils"
+)
+
+/*
+查找数组中某个value, 天天用到
+*/
+func linearSearch(arr []int, n int) {
+	for i, i2 := range arr {
+		if i2 == n {
+			fmt.Println(i)
+		}
+	}
+}
+func main() {
+	arr := utils.GetArrayOfLenAndSize(5, 100)
+	linearSearch(arr, arr[3])
+}
