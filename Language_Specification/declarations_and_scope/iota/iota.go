86a397f19d1aff2872e712db4a45cd081bf10103
zhaoyl <htzhao200744@gmail.com>
2020-06-16 17:33:47 +0800

    [language_specification]  update iota.go

--- Language_Specification/declarations_and_scope/iota/iota.go
+++ Language_Specification/declarations_and_scope/iota/iota.go
@@ -3,18 +3,27 @@ package main
 import "fmt"
 
 const (
-	c0 = iota // c0 == 0
+	c0 = iota // c0 == 0 ， iota 自增
 	c1 = iota // c1 == 1
 	c2 = iota // c2 == 2
 )
-
+const (
+	c00 = iota // c0 == 0 ， iota自增
+	c01
+	c02
+)
 const (
 	a = 1 << iota // a == 1  (iota == 0)
-	b = 1         // b == 2  (iota == 1)
+	b = 1         // b == 1  (iota == 1，unused)
 	c = 3         // c == 3  (iota == 2, unused)
-	d = 1         // d == 8  (iota == 3)
+	d = 1         // d == 1  (iota == 3，unused)
 )
-
+const (
+	aa = 1 << iota // 左移 ， 1
+	bb             // 10 = 2
+	cc             // 100 = 4
+	dd             // 1000 = 8
+)
 const (
 	u         = iota * 42 // u == 0     (untyped integer constant)
 	v float64 = iota * 42 // v == 42.0  (float64 constant)
@@ -25,7 +34,9 @@ const x = iota // x == 0
 const y = iota // y == 0
 func main() {
 	fmt.Println(c0, c1, c2)
+	fmt.Println(c00, c01, c02)
 	fmt.Println(a, b, c, d)
+	fmt.Println(aa, bb, cc, dd)
 	fmt.Println(u, v, w)
 	fmt.Println(x, y)
 }
