1bcad8738320897e56582718a5cc4e2b7405422f
zhaoyl <htzhao200744@gmail.com>
2020-06-28 21:48:30 +0800

    [golang] rm verdon

--- vendor/github.com/crgimenes/goconfig/doc.go
+++
@@ -1,8 +0,0 @@
-//Copyright (c) 2016 Cesar Gimenes - MIT License
-//
-//goconfig uses a struct as input and populates the fields
-//of this struct with parameters fom command line, environment variables
-//and configuration file.
-//
-
-package goconfig
--- vendor/github.com/crgimenes/goconfig/validate/doc.go
+++
@@ -1,5 +0,0 @@
-//Copyright (c) 2016 Cesar Gimenes - MIT License
-//
-//validate uses a struct
-
-package validate
