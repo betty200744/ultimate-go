c633d97c9b8477acbd4537965a96b1fc8981e9f6
zhaoyl <htzhao200744@gmail.com>
2020-07-08 22:11:54 +0800

    [golang] update insertion-sort.go

--- algorithms/sorting-algorithms/insertion-sort/insertion-sort.go
+++ algorithms/sorting-algorithms/insertion-sort/insertion-sort.go
@@ -10,17 +10,29 @@ import (
  */
 func insertionSort1(arr []int) {
 	// 左起第二张, 从左到右依次选出来比较
-	for j := 1; j < len(arr); j++ {
+	for i := 1; i < len(arr); i++ {
 		// 选出的牌
-		key := arr[j]
+		key := arr[i]
 		// "选出的牌"， 依次与"选中的牌的前面的牌"对比，
-		i := j - 1
-		for ; i >= 0 && arr[i] >= key; i-- {
+		j := i - 1
+		for ; j >= 0 && arr[j] >= key; j-- {
 			// 大则往后挪一位，
-			arr[i+1] = arr[i]
+			arr[j+1] = arr[j]
 		}
-		// 最后一个挪动的位置就是key选出的牌应该插入的位置， 因为i--了， 所有要加回来，
-		arr[i+1] = key
+		// 最后一个挪动的位置就是key选出的牌应该插入的位置， 因为k--了， 所有要加回来，
+		arr[j+1] = key
+	}
+	fmt.Println(arr)
+}
+
+func insertionSort2(arr []int) {
+	for i := 1; i < len(arr); i++ {
+		key := arr[i]
+		j := i - 1
+		for ; j >= 0 && arr[j] > key; j-- {
+			arr[j+1] = arr[j]
+		}
+		arr[j+1] = key
 	}
 	fmt.Println(arr)
 }
@@ -28,4 +40,5 @@ func insertionSort1(arr []int) {
 func main() {
 	arr := utils.GetArrayOfLenAndSize(8, 10)
 	insertionSort1(arr)
+	insertionSort2(arr)
 }
