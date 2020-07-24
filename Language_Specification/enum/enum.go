8765abfafdc4cd37949fd56dcb86c6ba5eb7c6b3
zhaoyl <htzhao200744@gmail.com>
2020-07-24 08:31:48 +0800

    [golang] update Language specification add enum

--- Language_Specification/enum/enum.go
+++ Language_Specification/enum/enum.go
@@ -30,11 +30,14 @@ var ACTION_TYPE_value = map[string]int32{
 	"ACTION_APPROACH": 4,
 }
 
-/*func (x ACTION_TYPE) String() string {
-	return proto.EnumName(ACTION_TYPE_name, int32(x))
-}*/
+func (x ACTION_TYPE) String() string {
+	return ACTION_TYPE_name[int32(x)]
+}
 
 func main() {
-	fmt.Println(ACTION_TYPE(3))
-	fmt.Println(ACTION_TYPE_ACTION_PICKUP)
+	fmt.Println("by const:", ACTION_TYPE_ACTION_PICKUP)
+	fmt.Println("by key:", ACTION_TYPE(3))
+	fmt.Println("by value", ACTION_TYPE_value["ACTION_PICKUP"])
+	fmt.Printf("enum:  %d \n", ACTION_TYPE_ACTION_PICKUP)
+	fmt.Printf("enum string:  %v \n", ACTION_TYPE_ACTION_PICKUP)
 }
