f77a3ac998df44d2d0fe53ad07d3bc633dc32369
zhaoyl <htzhao200744@gmail.com>
2020-07-26 15:07:44 +0800

    [Language_Specification] update arrays.go

--- Language_Specification/build-in-type/array/arrays.go
+++ Language_Specification/build-in-type/array/arrays.go
@@ -2,54 +2,53 @@ package main
 
 import (
 	"fmt"
-	"gobyexample/Language_Specification/greet"
 	"sort"
 )
 
 func main() {
 	//  init array, declare array, initial array, initial two Dimensional array, multi-line initial array
 	//  set array, get array, len of array , comparison array, append, sort, iteration array for range
-	var a [3]int     // declare
-	var aa [2][2]int // declare twoD array
-	aaa := []string{
-		"c",
-		"b",
-	} //  initial value, longhand var a [n]T = [n]T{V1,V2,...,Vn} , shorthand: a =: [n]T{V1, V2}
-
-	var aaaa []string //declare array
-	var aaaaa = new(int)
-	aaaaaa := [3]int{1, 2, 3}
-	aaaaaaa := [...]string{"aaa", "bbb"}
-	aaaaaaaa := [][]string{{"a1", "a2"}, {"b1", "b2"}}
-
-	a[0] = 2     // value assignment
-	aa[0][0] = 2 // value assignment
-	aaa = append(aaa, "a")
-	sort.Strings(aaa)
-	copy(aaaa, aaa)
-	for e := range aaa {
-		fmt.Println(e)
+	// Declare and initialize
+	var a1 [3]int
+	fmt.Println("this is a1: ", a1)
+	// Declare twoD array
+	var a2 [2][2]int
+	fmt.Println("this is a2: ", a2)
+	// Declare and initial value, longhand var a1 [n]T = [n]T{V1,V2,...,Vn} , shorthand: a1 =: [n]T{V1, V2}
+	a3 := []string{"cc", "dd"}
+	fmt.Println("this is a3:", a3)
+	// Declare
+	var a4 []string //declare array
+	fmt.Printf("this is a4: %v , len : %v , cap: %v \n", a4, len(a4), cap(a4))
+	a5 := [...]string{"a3", "bbb"}
+	fmt.Printf("this is a5: %v , len : %v , cap: %v \n", a5, len(a5), cap(a5))
+	a6 := [][]string{{"a1", "a2"}, {"b1", "b2"}}
+	fmt.Printf("this is a6: %v , len : %v , cap: %v \n", a6, len(a6), cap(a6))
+	// value assignment
+	a1[0] = 2
+	a2[0][0] = 2
+	// Append
+	a3 = append(a3, "a1")
+	// Sort
+	sort.Strings(a3)
+	// Copy
+	copy(a4, a3)
+	// Iterate with range
+	for i, a := range a3 {
+		fmt.Printf("Iterate with range: index: %d, value: %v \n", i, a)
+	}
+	// Iterate with traditional style
+	for i := 0; i < len(a3); i++ {
+		fmt.Printf("Iterate with traditional style: index: %d, value: %v \n", i, a3[i])
 	}
 
-	greett.Greet()
-
-	fmt.Println(a, a[0], aa, aa[0][0], len(a), aaa, aaaa, aaaaa, aaaaaa, aaaaaaa, aaaaaaaa, a == aaaaaa)
-
-	// array
-	var a [2]string
-	a[0] = "hello"
-	a[1] = "world"
-	fmt.Println(a)
-	b := [6]int{1, 2, 3, 4, 5, 6}
-	reviews := [2]Review{{1, "r1"}}
-	fmt.Println(b)
-	fmt.Println(reviews)
-
-	// multi array , 类型为[]string的array[]
-	arrays := [][]string{
-		[]string{"1", "11"},
-		[]string{"2", "22"},
+	// -----------------------------
+	// Contiguous memory allocations
+	// -----------------------------
+	// Declare an array of 6 strings initialized with values.
+	a7 := [6]string{"Annie", "Betty", "Charley", "Doug", "Edward", "Hoanh"}
+	fmt.Printf("\n=> Contiguous memory allocations\n")
+	for i, v := range a7 {
+		fmt.Printf("Value[%s]\tAddress[%p] IndexAddr[%p]\n", v, &v, &a7[i])
 	}
-	fmt.Println(arrays)
-
 }
