7cd060c5dc992ad83b1c992f81516ad18a9c955c
zhaoyl <htzhao200744@gmail.com>
2020-07-31 14:18:15 +0800

    [concurrency_visualize] add ping pong channel

--- LearnConcurrency/concurrency/concurrency_visualize/ping_pong.go
+++ LearnConcurrency/concurrency/concurrency_visualize/ping_pong.go
@@ -1,1 +1,12 @@
 package main
+
+func Ping(chping chan<- string) {
+	chping <- "send ping"
+	close(chping)
+}
+
+func Pong(chping <-chan string, chpong chan<- string) {
+	msg := "receive: " + <-chping + ", then send pong!"
+	chpong <- msg
+	close(chpong)
+}
--- LearnConcurrency/concurrency/concurrency_visualize/ping_pong_test.go
+++ LearnConcurrency/concurrency/concurrency_visualize/ping_pong_test.go
@@ -1,1 +1,10 @@
 package main
+
+import "testing"
+
+func TestPing_Pong(t *testing.T) {
+	pings := make(chan string, 1)
+	pongs := make(chan string, 1)
+	Ping(pings)
+	Pong(pings, pongs)
+}
