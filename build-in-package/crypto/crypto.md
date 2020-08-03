4a81e61f4e616d33a6b910b555f53cf4ec67f505
zhaoyl <htzhao200744@gmail.com>
2020-08-03 10:19:04 +0800

    [awesome-go] add cipher.go

--- build-in-package/crypto/crypto.md
+++ build-in-package/crypto/crypto.md
@@ -1,11 +1,31 @@
 
 - **Hashing** [wiki](https://en.wikipedia.org/wiki/List_of_hash_functions)
+*  not reversible, 不可逆
+
 
 - **Encryption or Ciphers**
+   - aes, [code](./encryption_cipher/aes/aes.go)
+   - cipher, [code](./encryption_cipher/cipher/cipher.go)  
+   - des, [code](./encryption_cipher) 
+   - rc4, [code](./encryption_cipher)
+   - rsa, [code](./encryption_cipher)
+
 ![](../../img/ciphers.png)
-* 可Decryption
+```
+* 有Encryption
+* 有Decryption
+* Caesar shift
+* stream cipher
+```
+
+
+
 
 - **Salting**
+![](../../img/salting.png)
+*  not reversible, 不可逆
+*  先添加一个random string, 之后hash
+
 
 - **Encoding**
 * 有Decode , 有Encode 
---
+++ build-in-package/crypto/encryption_cipher/cipher/cipher.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"io"
+)
+
+func main() {
+	key, _ := hex.DecodeString("6368616e676520746869732070617373")
+	//Encryption
+	plaintext := []byte("exampleplaintext")
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		panic(err)
+	}
+	modeE := cipher.NewCBCEncrypter(block, iv)
+	modeE.CryptBlocks(ciphertext[aes.BlockSize:], plaintext)
+	fmt.Printf("%x\n", ciphertext)
+
+	//Decryption
+	iv = ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+	// CBC modeE always works in whole blocks.
+	if len(ciphertext)%aes.BlockSize != 0 {
+		panic("ciphertext is not a multiple of the block size")
+	}
+	modeD := cipher.NewCBCDecrypter(block, iv)
+	// CryptBlocks can work in-place if the two arguments are the same.
+	modeD.CryptBlocks(ciphertext, ciphertext)
+	fmt.Printf("%s\n", ciphertext)
+}
