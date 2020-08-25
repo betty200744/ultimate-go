5814620172b2a2de97bce48423b6379c16f8c877
zhaoyl <htzhao200744@gmail.com>
2020-08-25 21:20:25 +0800

    [algorithms][data-structures] stack add max method

--- algorithms/data-structures/stack/stack.go
+++ algorithms/data-structures/stack/stack.go
@@ -1,5 +1,7 @@
 package stack
 
+import "reflect"
+
 // https://en.wikipedia.org/wiki/Stack_%28abstract_data_type%29#Array
 // stacks 如同叠盘子， 先进后出
 
@@ -42,4 +44,25 @@ func (s *Stack) Pop() interface{} {
 	s.stack = s.stack[1:]
 	s.len--
 	return el
+}
+func (s *Stack) Max() interface{} {
+	if s.IsEmpty() {
+		return 0
+	}
+	max := s.stack[0]
+	for i := 0; i < s.len; i++ {
+		switch v := reflect.ValueOf(s.stack[i]); v.Kind() {
+		case reflect.String:
+			if s.stack[i].(string) > max.(string) {
+				max = s.stack[i]
+			}
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			if s.stack[i].(int) > max.(int) {
+				max = s.stack[i]
+			}
+		default:
+			max = 0
+		}
+	}
+	return max
 }
