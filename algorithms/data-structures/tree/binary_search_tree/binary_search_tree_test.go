8bf10efedd4a91c5b7070b22f2023f92129fec92
zhaoyl <htzhao200744@gmail.com>
2020-08-28 19:04:30 +0800

    [algorithms] update BreadthFirst

--- algorithms/data-structures/tree/binary_search_tree/binary_search_tree_test.go
+++ algorithms/data-structures/tree/binary_search_tree/binary_search_tree_test.go
@@ -40,3 +40,38 @@ func TestNode_FindMax(t *testing.T) {
 	tree.Insert(1)
 	tree.FindMax()
 }
+func TestNode_KthSmallest(t *testing.T) {
+	tree := NewNode(4)
+	tree.Insert(1)
+	tree.Insert(2)
+	tree.Insert(3)
+	tree.Insert(5)
+	tree.Insert(6)
+	tree.Insert(7)
+	assert.Equal(t, 1, KthSmallest(tree, 1))
+	assert.Equal(t, 4, KthSmallest(tree, 4))
+	assert.Equal(t, 6, KthSmallest(tree, 6))
+}
+
+func TestGetSize(t *testing.T) {
+	tree := NewNode(4)
+	tree.Insert(1)
+	tree.Insert(2)
+	tree.Insert(3)
+	tree.Insert(5)
+	tree.Insert(6)
+	tree.Insert(7)
+	assert.Equal(t, CountNode(tree), 7)
+	assert.Equal(t, CountNode(tree.Left), 3)
+	assert.Equal(t, CountNode(tree.Right), 3)
+}
+func TestBreadthFirst(t *testing.T) {
+	tree := NewNode(4)
+	tree.Insert(1)
+	tree.Insert(2)
+	tree.Insert(3)
+	tree.Insert(5)
+	tree.Insert(6)
+	tree.Insert(7)
+	BreadthFirst(tree)
+}
