d2d45cfb1c2884a019f1c1852d205e8b34d1e371
zhaoyl <htzhao200744@gmail.com>
2020-08-29 08:03:23 +0800

    [leetcode][lcof] add KthBiggest

--- leetcode/binary_search/230.Kth-Smallest-Element-in-a-BST/230_KthSmallest_test.go
+++ leetcode/binary_search/230.Kth-Smallest-Element-in-a-BST/230_KthSmallest_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestNode_KthSmallest(t *testing.T) {
+func TestKthSmallest(t *testing.T) {
 	tree := NewNode(4)
 	tree.Insert(1)
 	tree.Insert(2)
@@ -16,4 +16,16 @@ func TestNode_KthSmallest(t *testing.T) {
 	assert.Equal(t, 1, KthSmallest(tree, 1))
 	assert.Equal(t, 4, KthSmallest(tree, 4))
 	assert.Equal(t, 6, KthSmallest(tree, 6))
+}
+func TestKthBiggest(t *testing.T) {
+	tree := NewNode(4)
+	tree.Insert(1)
+	tree.Insert(2)
+	tree.Insert(3)
+	tree.Insert(5)
+	tree.Insert(6)
+	tree.Insert(7)
+	assert.Equal(t, 7, KthBiggest(tree, 1))
+	assert.Equal(t, 4, KthBiggest(tree, 4))
+	assert.Equal(t, 6, KthBiggest(tree, 2))
 }
