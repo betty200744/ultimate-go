03c9f6ead9858eb140bbd9dff496611653336484
zhaoyl <htzhao200744@gmail.com>
2020-09-02 08:53:09 +0800

    [leetcode] add 102_107_levelOrder.go

---
+++ leetcode/binary_search/102_107_levelOrder/102_107_levelOrder_test.go
@@ -0,0 +1,48 @@
+package _02_107_levelOrder
+
+import (
+	"fmt"
+	"gopkg.in/go-playground/assert.v1"
+	"testing"
+)
+
+func TestHeight(t *testing.T) {
+	root := NewNode(3)
+	root.Left = NewNode(9)
+	root.Right = NewNode(20)
+	root.Right.Left = NewNode(15)
+	root.Right.Right = NewNode(7)
+	res := Height(root)
+	assert.Equal(t, 3, res)
+}
+func TestGivenLevelOrder(t *testing.T) {
+	root := NewNode(3)
+	root.Left = NewNode(9)
+	root.Left.Left = NewNode(8)
+	root.Left.Right = NewNode(10)
+	root.Right = NewNode(20)
+	root.Right.Left = NewNode(15)
+	root.Right.Right = NewNode(7)
+	res := GivenLevelOrder(root, 2)
+	fmt.Println(res)
+	res = GivenLevelOrder(root, 1)
+	fmt.Println(res)
+}
+func TestLevelOrder(t *testing.T) {
+	root := NewNode(3)
+	root.Left = NewNode(9)
+	root.Right = NewNode(20)
+	root.Right.Left = NewNode(15)
+	root.Right.Right = NewNode(7)
+	res := LevelOrder(root)
+	fmt.Println(res)
+}
+func TestLevelOrderBottom(t *testing.T) {
+	root := NewNode(3)
+	root.Left = NewNode(9)
+	root.Right = NewNode(20)
+	root.Right.Left = NewNode(15)
+	root.Right.Right = NewNode(7)
+	res := LevelOrderBottom(root)
+	fmt.Println(res)
+}
