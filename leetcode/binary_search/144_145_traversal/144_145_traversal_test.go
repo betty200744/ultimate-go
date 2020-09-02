d679953d67a0897acc92497c223a74220a15f51e
zhaoyl <htzhao200744@gmail.com>
2020-09-02 09:57:26 +0800

    [leetcode] add 144_145_traversal.go

---
+++ leetcode/binary_search/144_145_traversal/144_145_traversal_test.go
@@ -0,0 +1,28 @@
+package _44_145_traversal
+
+import (
+	"gopkg.in/go-playground/assert.v1"
+	"testing"
+)
+
+func TestPreOrder(t *testing.T) {
+	root := NewTreeNode(1)
+	root.Right = NewTreeNode(2)
+	root.Right.Left = NewTreeNode(3)
+	res := PreOrder(root)
+	assert.Equal(t, []int{1, 2, 3}, res)
+}
+func TestPostOrder(t *testing.T) {
+	root := NewTreeNode(1)
+	root.Right = NewTreeNode(2)
+	root.Right.Left = NewTreeNode(3)
+	res := PostOrder(root)
+	assert.Equal(t, []int{3, 2, 1}, res)
+}
+func TestInOrder(t *testing.T) {
+	root := NewTreeNode(1)
+	root.Right = NewTreeNode(2)
+	root.Right.Left = NewTreeNode(3)
+	res := InOrder(root)
+	assert.Equal(t, []int{1, 3, 2}, res)
+}
