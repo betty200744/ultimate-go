2d8cc4cf69a62ff5e54db758c58ebf9b59fcbdbf
zhaoyl <htzhao200744@gmail.com>
2020-09-04 10:25:11 +0800

    [leetcode] add 160_getIntersectionNode.go

---
+++ leetcode/linked_list/160_getIntersectionNode/160_getIntersectionNode.go
@@ -0,0 +1,26 @@
+package _60_getIntersectionNode
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func NewListNode(value int) *ListNode {
+	return &ListNode{
+		Val:  value,
+		Next: nil,
+	}
+}
+func GetIntersectionNode(headA, headB *ListNode) *ListNode {
+	for headA != nil {
+		tmpB := headB
+		for tmpB != nil {
+			if headA == tmpB {
+				return tmpB
+			}
+			tmpB = tmpB.Next
+		}
+		headA = headA.Next
+	}
+	return nil
+}
