8fc1111cc750dec6e61cc119844286818de0172f
zhaoyl <htzhao200744@gmail.com>
2020-09-06 08:18:29 +0800

    [leetcode] update

--- leetcode/array/509_fibonacciNumber/509_fibonacciNumber.go
+++ leetcode/array/509_fibonacciNumber/509_fibonacciNumber.go
@@ -6,12 +6,32 @@ func FibonacciNumber(N int) int {
 	}
 	return FibonacciNumber(N-1) + FibonacciNumber(N-2)
 }
-func FibonacciNumberDP(N int) int {
-	f := make([]int, N+1)
-	f[0] = 0
-	f[1] = 1
-	for i := 2; i <= N; i++ {
-		f[i] = f[i-1] + f[i-2]
+
+// the best, Bottom-up DP Algorithm
+func Fib_DP_Tabulation(N int) int {
+	arr := make([]int, N+1)
+	if N < 1 {
+		arr[0] = 0
+	} else {
+		arr[0] = 0
+		arr[1] = 1
 	}
-	return f[N]
+	for i := 2; i <= N; i++ {
+		arr[i] = arr[i-1] + arr[i-2]
+	}
+	return arr[N]
+}
+
+// bad
+func Fib_DP_memoized(N int) int {
+	arr := make([]int, N+1)
+	if arr[N] != 0 {
+		return arr[N]
+	}
+	if N <= 1 {
+		return N
+	} else {
+		arr[N] = Fib_DP_memoized(N-1) + Fib_DP_memoized(N-2)
+	}
+	return arr[N]
 }
