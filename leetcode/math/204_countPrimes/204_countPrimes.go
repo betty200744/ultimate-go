347acb18fd221844759c4a1c9120842d3db72540
zhaoyl <htzhao200744@gmail.com>
2020-09-07 20:14:57 +0800

    [leetcode] update 204_countPrimes.go

--- leetcode/math/204_countPrimes/204_countPrimes.go
+++ leetcode/math/204_countPrimes/204_countPrimes.go
@@ -6,7 +6,10 @@ import (
 )
 
 func IsPrimes(n int) bool {
-	if n%2 == 0 {
+	if n < 2 {
+		return false
+	}
+	if n != 2 && n%2 == 0 {
 		return false
 	}
 	for i := 3; i <= int(math.Sqrt(float64(n))); i += 2 {
@@ -17,7 +20,7 @@ func IsPrimes(n int) bool {
 	return true
 }
 func CountPrimes(n int) int {
-	if n <= 2 {
+	if n < 2 {
 		return 0
 	}
 	arr := make([]int, 0)
@@ -28,4 +31,26 @@ func CountPrimes(n int) int {
 	}
 	fmt.Println(arr)
 	return len(arr)
+}
+func GenePrimes(n int) []int {
+	arr := make([]int, 0)
+	for i := 1; i < n; i++ {
+		if IsPrimes(i) {
+			arr = append(arr, i)
+		}
+	}
+	return arr
+}
+func GapPrimes(primes []int, k int) []int {
+	gaps := make([]int, 0)
+	for i := 0; i < len(primes)-1; i++ {
+		gaps = append(gaps, primes[i+1]-primes[i])
+	}
+	arr := make([]int, 0)
+	for i, i2 := range gaps {
+		if i2 < k {
+			arr = append(arr, i+1)
+		}
+	}
+	return arr
 }
--- leetcode/math/204_countPrimes/204_countPrimes_test.go
+++ leetcode/math/204_countPrimes/204_countPrimes_test.go
@@ -1,16 +1,26 @@
 package _04_countPrimes
 
 import (
+	"fmt"
 	"gopkg.in/go-playground/assert.v1"
 	"testing"
 )
 
 func TestIsPrimes(t *testing.T) {
-	assert.Equal(t, true, IsPrimes(1))
+	assert.Equal(t, false, IsPrimes(1))
 }
 func TestCountPrimes(t *testing.T) {
 	res := CountPrimes(10)
-	assert.Equal(t, 4, res)
+	assert.Equal(t, 3, res)
 	res = CountPrimes(2)
 	assert.Equal(t, 0, res)
+}
+func TestGenePrimes(t *testing.T) {
+	res := GenePrimes(10)
+	fmt.Println(res)
+}
+func TestGapPrimes(t *testing.T) {
+	arr := GenePrimes(100)
+	res := GapPrimes(arr, 3)
+	fmt.Println(res)
 }
