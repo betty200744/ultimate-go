5620338e68ebd16dd633c7ce1dcb4a916977cb67
zhaoyuanli <zhaoyuanli@dobest.com>
2020-11-08 08:55:03 +0800

    [utils] add encrypt.go

--- utils/encrypt_test.go
+++ utils/encrypt_test.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 	"testing"
 
-	"gopkg.in/go-playground/assert.v1"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func TestMd5Hex(t *testing.T) {
@@ -23,4 +24,28 @@ func TestMd5Hex1(t *testing.T) {
 func TestMd5Base64(t *testing.T) {
 	s1 := Md5Base64([]byte(`aaa`))
 	fmt.Println(s1)
+}
+
+func Test_bcrypt(t *testing.T) {
+	passwork := "a"
+	b, _ := bcrypt.GenerateFromPassword([]byte("a"), 1)
+	fmt.Println(string(b))
+	fmt.Println(bcrypt.Cost(b))
+	err := bcrypt.CompareHashAndPassword(b, []byte(passwork))
+	assert.Empty(t, err)
+}
+
+func TestSha1(t *testing.T) {
+	s := Sha1([]byte("a"))
+	fmt.Println(s)
+}
+
+func TestSha256(t *testing.T) {
+	s := Sha256([]byte("a"))
+	fmt.Println(s)
+}
+
+func TestSha512(t *testing.T) {
+	s := Sha512([]byte("a"))
+	fmt.Println(s)
 }
