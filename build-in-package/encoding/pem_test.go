ee112354ba369e844f44cd02852f78407eccf5e7
zhaoyuanli <zhaoyuanli@dobest.com>
2020-11-09 09:03:11 +0800

    [utils] add encrypt.go

--- build-in-package/encoding/pem_test.go
+++ build-in-package/encoding/pem_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Test_pem_decode(t *testing.T) {
-	block, _ := pem.Decode([]byte(transKey2Pem(AppPrivateKeyZfb, "-----BEGIN RSA PRIVATE KEY-----\n", "-----END RSA PRIVATE KEY-----\n")))
+	block, _ := pem.Decode([]byte(ToPem(Key, "-----BEGIN RSA PRIVATE KEY-----\n", "-----END RSA PRIVATE KEY-----\n")))
 	fmt.Println(block.Type)
 	fmt.Println(block.Headers)
 }
