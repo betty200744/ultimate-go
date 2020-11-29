26d3138e4621c093bda423198e4434d753d377ce
zhaoyuanli <zhaoyuanli@dobest.com>
2020-11-29 20:03:17 +0800

    [time] tickers.go

--- build-in-package/time/tickers.go
+++ build-in-package/time/tickers.go
@@ -5,13 +5,16 @@ import (
 	"time"
 )
 
-func main() {
-	ticker := time.NewTicker(time.Second)
-	defer ticker.Stop()
-	go func() {
-		for t := range ticker.C {
-			fmt.Println(t)
-		}
-	}()
-	time.Sleep(time.Second * 5)
+func TickerTimeout(timeout time.Duration) {
+	ticker := time.NewTicker(timeout)
+	select {
+	case <-ticker.C:
+		fmt.Println("NewTicker timeout")
+	}
+}
+func TimeAfterTimeout(timeout time.Duration) {
+	select {
+	case <-time.After(timeout):
+		fmt.Println("time.After timeout")
+	}
 }
