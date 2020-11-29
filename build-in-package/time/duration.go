27846e35ff634e3f20edacc40861f753278342b8
zhaoyuanli <zhaoyuanli@dobest.com>
2020-11-29 19:17:39 +0800

    [time] duration.go

---
+++ build-in-package/time/duration.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func TimeDuration() {
+	// time.Duration
+	now := time.Now()
+	time.Sleep(time.Second)
+	duration := time.Since(now)
+	fmt.Println("this is Duration.String, Seconds:", duration.String())
+	fmt.Println("this is Duration.Hours:", duration.Hours())
+	fmt.Println("this is Duration.Minutes:", duration.Minutes())
+	fmt.Println("this is Duration.Seconds:", duration.Seconds())
+}
