9614b378305c661c5ebb083dadb68ede39bede07
zhaoyuanli <zhaoyuanli@dobest.com>
2020-12-06 15:07:24 +0800

    [build-in] time

--- build-in-package/time/timers.go
+++ build-in-package/time/timers.go
@@ -1,11 +1,11 @@
-package main
+package time
 
 import (
 	"fmt"
 	"time"
 )
 
-func main() {
+func TimeOutByTimer() {
 	timer1 := time.NewTimer(time.Second * 2)
 	fmt.Println("timer1 is expired", <-timer1.C)
 
@@ -16,5 +16,20 @@ func main() {
 	isStop := timer2.Stop()
 	if isStop {
 		fmt.Println("timer2 is stop!")
+	}
+}
+
+func TimeOutByChannel() {
+	c1 := make(chan string, 1)
+	go func() {
+		time.Sleep(time.Second * 2)
+		c1 <- "result 1"
+	}()
+
+	select {
+	case res := <-c1:
+		fmt.Println(res)
+	case <-time.After(time.Second):
+		fmt.Println("time out 1")
 	}
 }
