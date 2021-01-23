4fd7b0075d7610ac9022bc5710bc2539487d0003
zhaoyuanli <zhaoyuanli@dobest.com>
2021-01-23 14:26:12 +0800

    [rabbitmq] add simple producer.go, consumer.go

---
+++ awesome-go/rabbitmq/rabbitmq.go
@@ -0,0 +1,46 @@
+package rabbitmq
+
+import (
+	"github.com/streadway/amqp"
+)
+
+var (
+	defaultUrl  = "amqp://guest:guest@localhost:5672/"
+	connections map[string]*amqp.Connection
+)
+
+type RabbitMQ struct {
+}
+
+func New() *RabbitMQ {
+	connections = map[string]*amqp.Connection{}
+	r := &RabbitMQ{}
+	return r
+}
+
+func (r *RabbitMQ) connection(url string) *amqp.Connection {
+	if url == "" {
+		url = defaultUrl
+	}
+	if conn, ok := connections[url]; ok {
+		return conn
+	}
+	conn, err := amqp.Dial(url)
+	if err != nil {
+		return nil
+	}
+	connections[url] = conn
+	return conn
+}
+func (r *RabbitMQ) GetChannel() (channel *amqp.Channel, err error) {
+	conn := r.connection("")
+	channel, err = conn.Channel()
+	if err != nil {
+		return nil, err
+	}
+	return channel, nil
+}
+
+func (r *RabbitMQ) send(channel *amqp.Channel, name string, route string, message []byte) error {
+	return nil
+}
