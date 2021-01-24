14854d1ff296f17ec97221e4fd739fdc8af68873
zhaoyuanli <zhaoyuanli@dobest.com>
2021-01-24 08:18:14 +0800

    [rabbitmq] add pub_sub , producer.go, consumer.go

---
+++ awesome-go/rabbitmq/routing/producer/producer.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"gobyexample/awesome-go/rabbitmq"
+	"gobyexample/utils"
+)
+
+func main() {
+	exchange := "amq.direct"
+	mq := rabbitmq.New()
+	ch, err := mq.GetChannel()
+	if err != nil {
+		panic(err)
+	}
+	err = ch.ExchangeDeclare(exchange, "direct", true, false, false, false, nil)
+	if err != nil {
+		return
+	}
+	for i := 0; i < 5; i++ {
+		mq.Send(ch, exchange, "", "review", utils.RandomString(8))
+		mq.Send(ch, exchange, "", "activity", utils.RandomString(8))
+		mq.Send(ch, exchange, "", "vip", utils.RandomString(8))
+	}
+}
