67c16c8a4395835197654665a4a106df28caf63c
zhaoyuanli <zhaoyuanli@dobest.com>
2021-06-05 07:31:19 +0800

    [awesome-go][influxdb] add write_data.go and write_data_test.go

---
+++ awesome-go/influxdb-client-go/basic_example/wirte_data.go
@@ -0,0 +1,72 @@
+package basic_example
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+)
+
+const (
+	Org         = "wuyun"
+	Bucket      = "stat"
+	DispatchDay = "dispatch_day"
+	HostStats   = "host_stats"
+	token       = "VmSIO0xg8lpLCBFthbt85ZMQ6_9sjaLRfOg0d7lgQOtv16SzJXzq7fxfVtpt3OjtBywWXQiINwcuY1wHDEwp8w=="
+)
+
+type HostStat struct {
+	Pid       string `json:"pid"`       // 资源池（虚拟机房）标识
+	Qid       string `json:"qid"`       // 资源池（虚拟机房）队列标识
+	Bside     string `json:"bside"`     // 业务方标识(1-小悟云 2-领沃)
+	Total     int64  `json:"total"`     // 总数
+	Live      int64  `json:"live"`      // 可用数
+	Used      int64  `json:"used"`      // 已使用数
+	UsedRate  int64  `json:"used_rate"` // 已使用数
+	Timestamp int64  `json:"timestamp"`
+}
+
+func getClient() influxdb2.Client {
+	return influxdb2.NewClient("http://localhost:8086", token)
+}
+
+func WriteHostStatsWithWritePoint(ctx context.Context, ctime time.Time) {
+	client := getClient()
+	writeApi := client.WriteAPIBlocking(Org, Bucket)
+	// write point with measurement, tags , fields
+	p := influxdb2.NewPoint(HostStats, map[string]string{"pid": "0", "qid": "1", "bside": "1"}, map[string]interface{}{"total": float64(1), "used": float64(3)}, ctime)
+	err := writeApi.WritePoint(ctx, p)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer client.Close()
+}
+func WriteHostStatsWithNewPointWithMeasurement(ctx context.Context, ctime time.Time) {
+	client := getClient()
+	writeApi := client.WriteAPIBlocking(Org, Bucket)
+	// write point only with measurement, then add tag, add field
+	pf := influxdb2.NewPointWithMeasurement(HostStats).
+		AddTag("pid", "0").
+		AddTag("qid", "0").
+		AddTag("bside", "2").
+		AddField("total", float64(6)).
+		AddField("used", float64(3)).
+		SetTime(ctime)
+	err := writeApi.WritePoint(ctx, pf)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer client.Close()
+}
+func WriteHostStatsWithLineProtocol(ctx context.Context, pid, qid, bside string, total, live, used float64, ctime int64) {
+	client := getClient()
+	writeApi := client.WriteAPIBlocking(Org, Bucket)
+	// write record using line protocol
+	line := fmt.Sprintf("%v,pid=%v,qid=%v,bside=%v total=%v,live=%v,used=%v %v", HostStats, pid, qid, bside, total, live, used, ctime)
+	err := writeApi.WriteRecord(ctx, line)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer client.Close()
+}
---
+++ awesome-go/influxdb-client-go/basic_example/wirte_data_test.go
@@ -0,0 +1,92 @@
+package basic_example
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestWriteHostStatsWithLineProtocol(t *testing.T) {
+	ctx := context.Background()
+	type args struct {
+		ctx   context.Context
+		pid   string
+		qid   string
+		bside string
+		total float64
+		live  float64
+		used  float64
+		ctime int64
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "line protocol 1",
+			args: args{ctx: ctx, pid: "1", qid: "1", bside: "1", total: 1, live: 1, used: 1, ctime: time.Now().UnixNano()},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			WriteHostStatsWithLineProtocol(tt.args.ctx, tt.args.pid, tt.args.qid, tt.args.bside, tt.args.total, tt.args.live, tt.args.used, tt.args.ctime)
+			fmt.Println("first timestamp is: ", time.Unix(0, tt.args.ctime).Unix())
+			QueryLatestDate()
+		})
+	}
+}
+
+func TestWriteHostStatsWithWritePoint(t *testing.T) {
+	ctx := context.Background()
+	type args struct {
+		ctx   context.Context
+		ctime time.Time
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "TestWriteHostStatsWithWritePoint",
+			args: args{
+				ctx:   ctx,
+				ctime: time.Now(),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			WriteHostStatsWithWritePoint(tt.args.ctx, tt.args.ctime)
+			fmt.Println("first timestamp is: ", tt.args.ctime.Unix())
+			QueryLatestDate()
+		})
+	}
+}
+
+func TestWriteHostStatsWithNewPointWithMeasurement(t *testing.T) {
+	ctx := context.Background()
+	type args struct {
+		ctx   context.Context
+		ctime time.Time
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			name: "TestWriteHostStatsWithNewPointWithMeasurement",
+			args: args{
+				ctx:   ctx,
+				ctime: time.Now(),
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			WriteHostStatsWithNewPointWithMeasurement(tt.args.ctx, tt.args.ctime)
+			fmt.Println("first timestamp is: ", tt.args.ctime.Unix())
+			QueryLatestDate()
+		})
+	}
+}
