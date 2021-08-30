8a09c7fe2d201b755cdd3a208ef489422da91034
zhaoyuanli <zhaoyuanli@dobest.com>
2021-08-30 14:08:47 +0800

    [ultimate-go] up readme.md

--- main.go
+++ main.go
@@ -1,10 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type FooInterface interface {
+	bar() // method receiver is value then interface is value , method receiver is pointer then interface is pointer
+}
+
+//Won't throw a compile error. Because the bar method belongs to Foo struct.
+type Foo1 struct {
+}
+
+func (f Foo1) bar() {
+	panic("implement me")
+}
+
+//Won't throw a compile error. Because the bar method belongs to Foo struct.
+//Will throw a error. Because the bar method is bind to &Foo. And when compile checks whether Foo confirms to Foo interface, it will only check the required method existence.
+type Foo2 struct {
+}
+
+func (f *Foo2) bar() {
+	panic("implement me")
+}
+
+func funcName(foo FooInterface) {
+	fmt.Println(foo)
+}
 
 func main() {
-	fmt.Println("this is main")
-	{
-		fmt.Println("ddd")
-	}
+	funcName(Foo1{})  // Foo1.bar receiver is value , then Foo1 need value
+	funcName(&Foo2{}) // Foo2.bar receiver is pointer, then Foo2 need pointer
 }
