392689940241f8b449b18adf918dd321f6ca5b25
zhaoyl <htzhao200744@gmail.com>
2022-08-25 21:20:25 +0800

    [algorithms][data-structures] stack add max method

--- algorithms/data-structures/stack/stack_test.go
+++ algorithms/data-structures/stack/stack_test.go
@@ -2,6 +2,7 @@ package stack
 
 import (
 	"fmt"
+	"gopkg.in/go-playground/assert.v1"
 	"testing"
 )
 
@@ -16,4 +17,22 @@ func Test_Stacks(t *testing.T) {
 	fmt.Println(s.Pop())
 	fmt.Println(s.Pop())
 	fmt.Println(s.IsEmpty())
+}
+func TestStack_Max(t *testing.T) {
+	t.Run("int", func(t *testing.T) {
+		s := New()
+		s.Push(1)
+		s.Push(2)
+		s.Push(3)
+		s.Push(4)
+		assert.Equal(t, 4, s.Max())
+	})
+	t.Run("string", func(t *testing.T) {
+		s := New()
+		s.Push("a")
+		s.Push("b")
+		s.Push("c")
+		s.Push("d")
+		assert.Equal(t, "d", s.Max())
+	})
 }
