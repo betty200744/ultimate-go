3cc9fc50b32e02d69ac9429fa11f3f0fedb760f3
zhaoyl <htzhao200744@gmail.com>
2022-10-05 15:50:55 +0800

    [leetcode] add 198_house_Robber.go

--- leetcode/dynamic_programming/198_house_Robber/198_house_Robber_test.go
+++ leetcode/dynamic_programming/198_house_Robber/198_house_Robber_test.go
@@ -6,8 +6,22 @@ import (
 )
 
 func TestRob(t *testing.T) {
-	res := Rob([]int{1, 2, 3, 1})
+	res := Rob([]int{1, 2, 3, 4, 5, 6, 7})
+	res = Rob([]int{1, 2, 3, 1})
 	assert.Equal(t, 4, res)
 	res = Rob([]int{2, 7, 9, 3, 1})
 	assert.Equal(t, 12, res)
+	res = Rob([]int{1, 3, 1, 3, 100})
+	res = Rob([]int{4, 1, 1, 5})
+	assert.Equal(t, 9, res)
+}
+func TestRobDP(t *testing.T) {
+	res := RobDP([]int{1, 2, 3, 4, 5, 6, 7})
+	res = RobDP([]int{1, 2, 3, 1})
+	assert.Equal(t, 4, res)
+	res = RobDP([]int{2, 7, 9, 3, 1})
+	assert.Equal(t, 12, res)
+	res = RobDP([]int{1, 3, 1, 3, 100})
+	res = RobDP([]int{4, 1, 1, 5})
+	assert.Equal(t, 9, res)
 }
