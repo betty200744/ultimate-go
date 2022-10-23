e19960ffca6813d68fb36c9c860dff4722e9e7f5
zhaoyuanli <zhaoyuanli@dobest.com>
2022-10-23 14:54:27 +0800

    [rabbitmq] add queue , task.go, work.go

---
+++ awesome-go/rabbitmq/queue/work/work.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	"gobyexample/awesome-go/rabbitmq"
+	"gobyexample/utils"
+)
+
+func main() {
+	consumerName := fmt.Sprintf("consumer_%v", utils.RandInt(10000, 100000))
+	mq := rabbitmq.New()
+	ch, err := mq.GetChannel()
+	if err != nil {
+		panic(err)
+	}
+	msgs, err := ch.Consume("queue1", consumerName, true, false, false, false, nil)
+	if err != nil {
+
+	}
+	go func() {
+		for msg := range msgs {
+			fmt.Println(fmt.Sprintf("consumer: %v, exchange: %v, routing_key: %v, msg: %v", consumerName, msg.Exchange, msg.RoutingKey, string(msg.Body[:])))
+		}
+	}()
+	for {
+
+	}
+}
