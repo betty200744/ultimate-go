befae12d7fcd0a813a5c762cd59b8be303838b5d
zhaoyuanli <zhaoyuanli@dobest.com>
2022-10-23 14:26:12 +0800

    [rabbitmq] add simple producer.go, consumer.go

---
+++ awesome-go/rabbitmq/simple/consumer/consumer.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"gobyexample/awesome-go/rabbitmq"
+)
+
+func main() {
+	mq := rabbitmq.New()
+	ch, err := mq.GetChannel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+	q, err := ch.QueueDeclare("simple", true, false, false, false, nil)
+	if err != nil {
+		panic(err)
+	}
+	msgs, err := ch.Consume(q.Name, "", true, false, false, false, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	go func() {
+		for msg := range msgs {
+			fmt.Println(fmt.Sprintf("exchange: %v, routing_key: %v, msg: %v", msg.Exchange, msg.RoutingKey, string(msg.Body[:])))
+		}
+	}()
+	for {
+
+	}
+}
