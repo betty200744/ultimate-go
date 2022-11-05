98be8ee60b1cadd5c3922ea188606cb863116add
zhaoyuanli <zhaoyuanli@dobest.com>
2022-11-06 07:34:36 +0800

    [awesome] add rpcx demo

--- awesome-go/rpcx/101basic/client/client.go
+++ awesome-go/rpcx/101basic/client/client.go
@@ -11,11 +11,15 @@ import (
 
 func main() {
 	d, _ := client.NewPeer2PeerDiscovery("tcp@localhost:8972", "")
-	arithClient := client.NewXClient("Arith", client.Failtry, client.RandomSelect, d, client.DefaultOption)
+	xClient := client.NewXClient("Arith", client.Failtry, client.RandomSelect, d, client.DefaultOption)
 	reply := &arith.Reply{}
-	err := arithClient.Call(context.Background(), "Mul", &arith.Args{A: 1, B: 2}, reply)
+	err := xClient.Call(context.Background(), "Mul", &arith.Args{A: 1, B: 2}, reply)
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println("##############", reply)
+	err = xClient.Call(context.Background(), "Add", &arith.Args{A: 1, B: 2}, reply)
+	fmt.Println("##############", reply)
+	err = xClient.Call(context.Background(), "Say", "come back home", reply)
 	fmt.Println("##############", reply)
 }
