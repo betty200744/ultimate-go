6b85cd18c7ec0d48fec10f504e4792d34d558d2c
zhaoyuanli <zhaoyuanli@dobest.com>
2022-11-08 14:58:38 +0800

    [utils] array.go

---
+++ utils/array.go
@@ -0,0 +1,98 @@
+package utils
+
+import (
+	"reflect"
+	"strings"
+	"unicode"
+)
+
+func InArray(val interface{}, array interface{}) bool {
+	switch reflect.TypeOf(array).Kind() {
+	case reflect.Slice:
+		s := reflect.ValueOf(array)
+
+		for i := 0; i < s.Len(); i++ {
+			if reflect.DeepEqual(val, s.Index(i).Interface()) == true {
+
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func IsArray(val interface{}) bool {
+	rt := reflect.TypeOf(val)
+	switch rt.Kind() {
+	case reflect.Slice:
+		return true
+	case reflect.Array:
+		return true
+	default:
+		return false
+	}
+
+}
+
+func ArrayIsEmpty(arr interface{}) bool {
+	switch arr.(type) {
+	case []int, []int8, []int16, []int32, []int64, []uint, []uint8, []uint16, []uint32, []uint64, []string, []interface{}:
+		if reflect.ValueOf(arr).Len() > 0 {
+			return false
+		} else {
+			return true
+		}
+	default:
+		return true
+	}
+	return true
+}
+
+func CamelCaseToUnderscore(str string) string {
+	var output []rune
+	var segment []rune
+	for _, r := range str {
+
+		// not treat number as separate segment
+		if !unicode.IsLower(r) && string(r) != "_" && !unicode.IsNumber(r) {
+			output = addSegment(output, segment)
+			segment = nil
+		}
+		segment = append(segment, unicode.ToLower(r))
+	}
+	output = addSegment(output, segment)
+	return string(output)
+}
+
+func UnderscoreToCamelCase(inputUnderScoreStr string) (camelCase string) {
+	isToUpper := false
+	for k, v := range inputUnderScoreStr {
+		if k == 0 {
+			camelCase = strings.ToUpper(string(inputUnderScoreStr[0]))
+		} else {
+			if isToUpper {
+				camelCase += strings.ToUpper(string(v))
+				isToUpper = false
+			} else {
+				if v == '_' {
+					isToUpper = true
+				} else {
+					camelCase += string(v)
+				}
+			}
+		}
+	}
+	return
+}
+
+func addSegment(inrune, segment []rune) []rune {
+	if len(segment) == 0 {
+		return inrune
+	}
+	if len(inrune) != 0 {
+		inrune = append(inrune, '_')
+	}
+	inrune = append(inrune, segment...)
+	return inrune
+}
