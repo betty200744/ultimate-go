379737bcd66b1777376b9be9f2e187b85bb6b54c
zhaoyuanli <zhaoyuanli@dobest.com>
2022-11-08 08:55:03 +0800

    [utils] add encrypt.go

--- utils/encrypt.go
+++ utils/encrypt.go
@@ -2,6 +2,9 @@ package utils
 
 import (
 	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
 	"encoding/base64"
 	"encoding/hex"
 )
@@ -16,9 +19,28 @@ func Md5Hex1(v []byte) string {
 	hashInBytes := md5.Sum(v)
 	return hex.EncodeToString(hashInBytes[:])
 }
+
 func Md5Base64(v []byte) string {
 	h := md5.New()
 	h.Write(v)
 	s1 := h.Sum(nil)
 	return base64.StdEncoding.EncodeToString(s1)
+}
+
+func Sha1(input []byte) string {
+	hash := sha1.New()
+	hash.Write(input)
+	return hex.EncodeToString(hash.Sum(nil))
+}
+
+func Sha256(input []byte) string {
+	hash := sha256.New()
+	hash.Write(input)
+	return hex.EncodeToString(hash.Sum(nil))
+}
+
+func Sha512(input []byte) string {
+	hash := sha512.New()
+	hash.Write(input)
+	return hex.EncodeToString(hash.Sum(nil))
 }
