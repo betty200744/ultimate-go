712b23463b4825395b74ae01be7b4180d2a13dfe
zhaoyuanli <zhaoyuanli@dobest.com>
2022-11-10 09:52:07 +0800

    [awesome-go] bazel up

--- awesome-go/bazel/go-tutorial/service/service.go
+++ awesome-go/bazel/go-tutorial/service/service.go
@@ -1,12 +1,19 @@
 package service
 
 import (
-	"github.com/gin-gonic/gin"
 	"gobyexample/awesome-go/bazel/go-tutorial/handler/publish"
+
+	"github.com/gin-gonic/gin"
 )
 
 func SetUpRouter() *gin.Engine {
 	engine := gin.Default()
-	engine.GET("/publish", publish.Publish)
+	engine.GET("/", publish.Publish)
+	return engine
+}
+
+func SetUpRouter1() *gin.Engine {
+	engine := gin.Default()
+	engine.GET("/", publish.Publish)
 	return engine
 }
