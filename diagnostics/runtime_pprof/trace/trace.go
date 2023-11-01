3001f77459b54336a47b2c6bad3bdb0eeacd1040
zhaoyuanli <zhaoyuanli@dobest.com>
2023-11-02 04:13:47 +0800

    [diagnostics] up

--- diagnostics/runtime_pprof/trace/trace.go
+++ diagnostics/runtime_pprof/trace/trace.go
@@ -8,16 +8,38 @@ import (
 	"sync"
 )
 
+// cpu performance
 func counter(wg *sync.WaitGroup) {
 	wg.Done()
-	slice := []int{0}
+	slice := make([]int, 0)
 	c := 1
 	for i := 0; i < 100000; i++ {
 		c = i + 1 + 2 + 3 + 4 + 5
 		slice = append(slice, c)
 	}
 }
+func counter1(wg *sync.WaitGroup) {
+	wg.Done()
+	slice := make([]int, 100000)
+	c := 1
+	for i := 0; i < 100000; i++ {
+		c = i + 1 + 2 + 3 + 4 + 5
+		slice[i] = c
+	}
+}
+func counter2(wg *sync.WaitGroup) {
+	wg.Done()
+	slice := [100000]int{}
+	c := 1
+	for i := 0; i < 100000; i++ {
+		c = i + 1 + 2 + 3 + 4 + 5
+		//slice = append(slice, c)
+		slice[i] = c
+	}
+}
 
+// go run trace.go
+// go tool trace trace.pprof
 func main() {
 	//runtime.GOMAXPROCS(1)
 	var traceProfile = flag.String("traceprofile", "trace.pprof", "write trace profile to file")
@@ -36,7 +58,7 @@ func main() {
 	var wg sync.WaitGroup
 	for i := 0; i < 8; i++ {
 		wg.Add(1)
-		go counter(&wg)
+		go counter2(&wg)
 	}
 	wg.Wait()
 }
